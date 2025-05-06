0322b3712d304795c4f0c07a7305da922fc1f26d
garlicxd <202402636@post.au.dk>
2025-05-06 22:49:09 +0200

    ResourceDataBase is singleton now

--- Assets/Resources/ResourcesDatabase.cs
+++ Assets/Resources/ResourcesDatabase.cs
@@ -1,11 +1,35 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 // Access resourceDatabase[ResourceType.Arts] to get the resource object
 // Add amount: resourceDatabase[ResourceType.Arts].AddAmount(10);
 // Add modifier: resourceDatabase[ResourceType.Arts].AddModifier(10, (optional mod1, mod2));
-public class ResourcesDatabase
+public class ResourcesDatabase : MonoBehaviour
 {
+    // Singleton instance
+    private static ResourcesDatabase instance;
+
+    // Property to access the singleton instance
+    public static ResourcesDatabase Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<ResourcesDatabase>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("ResourcesDatabase");
+                    instance = obj.AddComponent<ResourcesDatabase>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    // Dictionary to hold resources by their type
+
     private Dictionary<ResourceType, Resource> resources;
 
     public ResourcesDatabase()
