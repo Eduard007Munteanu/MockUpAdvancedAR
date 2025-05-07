5c006a4559a1d456eb9142bdff387a1604b911b5
garlicxd <gergelypeti02@gmail.com>
2025-05-07 13:38:58 +0200

    agreement added

---
+++ Assets/Resources/AgreementResource.cs
@@ -0,0 +1,48 @@
+// --- Hi ---
+// Remember to add the ResourceType to Resources.cs
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgreementResource : Resource
+{
+    private float lastAgreementAmount = 0f;
+
+    public AgreementResource(
+        float initialAmount = 0f,
+        float minAmount = 0f,
+        float maxAmount = 100f,
+        int cycleTicks = 1
+        ) : base(ResourceType.Agreement, initialAmount, minAmount, maxAmount, cycleTicks) // TODO: Update ResourceType
+    {
+        // thresholds = new Thresholds(new List<float> { /* ...threshold values... */ }, initialAmount);
+        // calculate initial agreement
+    }
+
+    protected override void onAmountChange(float delta)
+    {
+
+
+        lastAgreementAmount = amount;
+    }
+
+    protected override void onProductionChange(float delta)
+    {
+
+    }
+
+    protected override void onThresholdCrossed(int i, ThresholdCross dir)
+    {
+
+    }
+
+    protected override void onReachedMax(float excess) {
+
+    }
+    protected override void onReachedMin(float deficit) {
+
+    }
+
+    // To handle changes in the political axes 
+    // Listen to their actions
+}
