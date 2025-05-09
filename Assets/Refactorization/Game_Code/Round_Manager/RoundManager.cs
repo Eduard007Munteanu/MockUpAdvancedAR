67cefe8c5b50ceb4b9662b008b9b88be21ec150c
garlicxd <gergelypeti02@gmail.com>
2025-05-09 15:36:37 +0200

    small changes

--- Assets/Refactorization/Game_Code/Round_Manager/RoundManager.cs
+++ Assets/Refactorization/Game_Code/Round_Manager/RoundManager.cs
@@ -2,10 +2,11 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class RoundManager : MonoBehaviour{ //Here I will need to call the ticks for every ressource. 
+public class RoundManager : MonoBehaviour
+{ //Here I will need to call the ticks for every ressource. 
 
 
-    public static RoundManager Instance {get; private set;}
+    public static RoundManager Instance { get; private set; }
 
     private List<EnemyTile> enemyTiles;
 
@@ -13,30 +14,36 @@ public class RoundManager : MonoBehaviour{ //Here I will need to call the ticks 
 
     private int numberOfEnemiesToSpawn = 0;  //Hardcoded
 
-    private float timeToActivateRound = 20f; 
+    private float timeToActivateRound = 20f;
 
-    private float timer = 0f; 
+    private float timer = 0f;
 
-    private bool timerIncreaser = true; 
+    private bool timerIncreaser = true;
 
     private float timeToWait = 1f;
 
     private float tickTimer = 0f;
     private float tickTime = 1f;
 
+    private float numOfRounds = 0;
+
     private ResourceDatabase resources;
 
 
     void Awake()
     {
-        if (Instance != null && Instance != this) {
+        if (Instance != null && Instance != this)
+        {
             Debug.LogWarning("More than one BuildManager detected. Destroying duplicate.");
             Destroy(gameObject);
-        } else {
+        }
+        else
+        {
             Instance = this;
         }
 
-        while (resources == null){
+        while (resources == null)
+        {
             Debug.Log("Waiting for ResourceDatabase to be initialized...");
             resources = ResourceDatabase.Instance;
         }
@@ -46,51 +53,63 @@ public class RoundManager : MonoBehaviour{ //Here I will need to call the ticks 
 
     void Start()
     {
-        enemyTiles = new List<EnemyTile>();   
+        enemyTiles = new List<EnemyTile>();
     }
 
 
 
-    int getRound(){
+    int getRound()
+    {
         return roundNumber;
     }
 
 
 
-    void Update(){
-        if(timerIncreaser){
+    void Update()
+    {
+        if (timerIncreaser)
+        {
             Debug.Log("We are exactly at UpdateTime");
             UpdateTime();
-        } else{ // new round
+        }
+        else
+        { // new round
             roundNumber += 1;
             Debug.Log("We are exactly at SpawnMobs");
-            resources[ResourceType.EnemyMight].AddAmount(1f); // increase enemy might each round
+            resources[ResourceType.EnemyMight].AddAmount(5f); // increase enemy might each round
+            if (roundNumber >= numOfRounds) {resources[ResourceType.EnemyMight].AddAmount(10f);}
             SpawnMobs();
         }
     }
 
 
-    void UpdateTime(){
+    void UpdateTime()
+    {
         timer += Time.deltaTime;
-        if(timer >= timeToActivateRound){
+        if (timer >= timeToActivateRound)
+        {
             SwitchRoundToSpawnState();
         }
 
         tickTimer += Time.deltaTime;
-        if(tickTimer >= tickTime){
+        if (tickTimer >= tickTime)
+        {
             // Call the tick for every resource here
             resources.Tick();                                             //I comented it out. Leads to error.
             tickTimer = 0f; // Reset the timer after calling the tick
         }
     }
 
-    void SpawnMobs(){
-        
-        timeToWait -= Time.deltaTime; 
-        if(timeToWait <= 0f){
+    void SpawnMobs()
+    {
+
+        timeToWait -= Time.deltaTime;
+        if (timeToWait <= 0f)
+        {
             Debug.Log("timeToWait reached 0, attempting spawn...");
 
-            if(enemyTiles.Count == 0){
+            if (enemyTiles.Count == 0)
+            {
                 Debug.LogError("No enemyTile detected!");
                 return;
             }
@@ -100,25 +119,28 @@ public class RoundManager : MonoBehaviour{ //Here I will need to call the ticks 
             Debug.Log("Spawning from: " + randomTile.name);
             randomTile.SetCreateMobs(true);
 
-            numberOfEnemiesToSpawn --;
+            numberOfEnemiesToSpawn--;
             Debug.Log("NumberOfEnemiesToSpawn" + numberOfEnemiesToSpawn);
-            timeToWait = Random.Range(0,5);
+            timeToWait = Random.Range(0, 5);
         }
-        if(numberOfEnemiesToSpawn == 0){
+        if (numberOfEnemiesToSpawn == 0)
+        {
             timer = 0f;
             SwitchRoundToSpawnState();
-        }   
+        }
     }
 
-    void SwitchRoundToSpawnState(){
+    void SwitchRoundToSpawnState()
+    {
         timerIncreaser = !timerIncreaser;
 
-        if (timerIncreaser) {
-        // We just ended a spawn round, reset timer-related stuff
-        numberOfEnemiesToSpawn = 0;
-        timeToWait = 1f;
+        if (timerIncreaser)
+        {
+            // We just ended a spawn round, reset timer-related stuff
+            numberOfEnemiesToSpawn = 0;
+            timeToWait = 1f;
+        }
     }
-    }
 
 
 
@@ -127,7 +149,8 @@ public class RoundManager : MonoBehaviour{ //Here I will need to call the ticks 
 
 
 
-    public void AddEnemyTile(EnemyTile enemyTile){
+    public void AddEnemyTile(EnemyTile enemyTile)
+    {
         enemyTiles.Add(enemyTile);
     }
 
